6b7ee1e0e5b5789ff399eeb11f47522de2f33237
Brahmaiah Gunji <nanibrahmaiag@gmail.com>
2016-12-16 08:03:22 -0800

    Merge remote-tracking branch 'refs/remotes/origin/master' into konydash-brahma
    # Conflicts:
    #	jssrc/android/generated/appskins.js
    #	jssrc/android/generated/frmAdmitForm.js
    #	jssrc/android/generated/frmConfigurationAlerts.js
    #	jssrc/android/generated/frmDashBoard.js
    #	jssrc/android/generated/frmEditProfile.js
    #	masters/Master02a90633ee58a41.sm/CopyLabel0654298db22b04a.json
    #	masters/Master02a90633ee58a41.sm/CopyLabel08d5039c9b1934a.json
    #	masters/Master02a90633ee58a41.sm/FlexContainer0b649aae5ffd040.json
    #	masters/Master02a90633ee58a41.sm/Label024694653a6a04a.json
    #	masters/Master02a90633ee58a41.sm/flxAdmittCointer.json
    #	masters/Master02a90633ee58a41.sm/flxAlerttoneSettings.json
    #	masters/Master02a90633ee58a41.sm/flxConfigarationAlertsContainer.json
    #	masters/Master02a90633ee58a41.sm/flxDashBoardContainer.json
    
    #	masters/Master02a90633ee58a41.sm/flxDischargeNotificationContainer.json
    #	masters/Master02a90633ee58a41.sm/flxLineContainer.json
    #	masters/Master02a90633ee58a41.sm/flxLogoutContainer.json
    #	masters/Master02a90633ee58a41.sm/flxMenuContainer.json
    #	masters/Master02a90633ee58a41.sm/flxMenuItemsContainer.json
    #	masters/Master02a90633ee58a41.sm/flxMenuProfileDetailsContainer.json
    #	masters/Master02a90633ee58a41.sm/flxProfileContainer.json
    #	masters/Master02a90633ee58a41.sm/flxSupportContainer.json
    #	masters/Master02a90633ee58a41.sm/imgAdmit.json
    #	masters/Master02a90633ee58a41.sm/imgAlertToneSettings.json
    #	masters/Master02a90633ee58a41.sm/imgConfigureAdmissionAlertsl.json
    #	masters/Master02a90633ee58a41.sm/imgDischargeNotification.json
    #	masters/Master02a90633ee58a41.sm/imgLogout.json
    #	masters/Master02a90633ee58a41.sm/imgMenuProfilePic.json
    #	masters/Master02a90633ee58a41.sm/imgProfile.json
    #	masters/Master02a90633ee58a41.sm/imgSupport.json
    #	masters/Master02a90633ee58a41.sm/labelAdmit.json
    #	masters/Master02a90633ee58a41.sm/labelAlerttoneSettings.json
    #	masters/Master02a90633ee58a41.sm/labelConfigureAdmissionAlerts.json
    #	masters/Master02a90633ee58a41.sm/labelDashBoard.json
    #	masters/Master02a90633ee58a41.sm/labelDischargeNotification.json
    #	masters/Master02a90633ee58a41.sm/labelLogout.json
    #	masters/Master02a90633ee58a41.sm/labelProfile.json
    #	masters/Master02a90633ee58a41.sm/labelSupport.json
    #	masters/Master02a90633ee58a41.sm/labelUsername.json
    
    #	templates/mobile/segments/tempSegmentMenuItem.sm/imgMeniIcon.json~Conflict
    resolved and commited by brahma

--- studioactions/mobile/AS_Button_261618402deb4a79b4fe54af75019ada.js
+++ studioactions/mobile/AS_Button_261618402deb4a79b4fe54af75019ada.js
@@ -1,38 +1,34 @@
-function AS_Button_Validation_MandatoryFeilds(eventobject) {
-    return AS_Button_2d8730b20ada430e8123dab92eb2265d(eventobject);
-}
+function AS_Button_261618402deb4a79b4fe54af75019ada(eventobject) {
+    function SHOW_ALERT_ide_onClick_d415aee909bd44c0bde943cbe76d49ab_True() {}
 
-function AS_Button_2d8730b20ada430e8123dab92eb2265d(eventobject) {
-    function SHOW_ALERT__33538ece039647ab85b6eaa25d35d452_True() {}
-
-    function SHOW_ALERT__2fda2713c5fe40b19d30dbc075014a2c_True() {}
+    function SHOW_ALERT_ide_onClick_e7249dc5eb6b4ab8b93d06bec2c9d385_True() {}
     if ((frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerPatientInfoAdmitForm.TFFirstName.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerPatientInfoAdmitForm.TFLastName.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerAdmittingPreferencesAdmitForm.ListBoxAdmittingHospital.selectedKey == listboxdefaultkey) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerAdmittingPreferencesAdmitForm.ListBoxAdmittingGroup.selectedKey == listboxdefaultkey) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerAdmittingPreferencesAdmitForm.ListBoxPatienETA.selectedKey == listboxdefaultkey) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerPatientDiagnosisAdmitForm.TBChiefComplaint.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormTemparature.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormBP.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormDiastolic.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormPulse.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormO2Level.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormWeight.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormHeight.text == tbTempAdmitPage) || (frmAdmitForm.flxContainer1.flxBodyContainer.flexAdmitFormContainer.admitFormTabSection.TestpatientTab.patientMandatorySection.flxContainerVitalSignsAdmitForm.TBAdmitFormBodyMass.text == tbTempAdmitPage)) {
         setDisableSubmitButton.call(this);
 
-        function SHOW_ALERT__2fda2713c5fe40b19d30dbc075014a2c_Callback() {
-            SHOW_ALERT__2fda2713c5fe40b19d30dbc075014a2c_True()
+        function SHOW_ALERT_ide_onClick_e7249dc5eb6b4ab8b93d06bec2c9d385_Callback() {
+            SHOW_ALERT_ide_onClick_e7249dc5eb6b4ab8b93d06bec2c9d385_True()
         }
         kony.ui.Alert({
             "alertType": constants.ALERT_TYPE_ERROR,
             "alertTitle": "Error",
             "yesLabel": "OK",
             "message": "Please fill all the mandatory feilds",
-            "alertHandler": SHOW_ALERT__2fda2713c5fe40b19d30dbc075014a2c_Callback
+            "alertHandler": SHOW_ALERT_ide_onClick_e7249dc5eb6b4ab8b93d06bec2c9d385_Callback
         }, {
             "iconPosition": constants.ALERT_ICON_POSITION_LEFT
         })
     } else {
         resetAdmitForm.call(this);
 
-        function SHOW_ALERT__33538ece039647ab85b6eaa25d35d452_Callback() {
-            SHOW_ALERT__33538ece039647ab85b6eaa25d35d452_True()
+        function SHOW_ALERT_ide_onClick_d415aee909bd44c0bde943cbe76d49ab_Callback() {
+            SHOW_ALERT_ide_onClick_d415aee909bd44c0bde943cbe76d49ab_True()
         }
         kony.ui.Alert({
             "alertType": constants.ALERT_TYPE_INFO,
             "alertTitle": "Success",
             "yesLabel": "OK",
             "message": "Submitted Successfully",
-            "alertHandler": SHOW_ALERT__33538ece039647ab85b6eaa25d35d452_Callback
+            "alertHandler": SHOW_ALERT_ide_onClick_d415aee909bd44c0bde943cbe76d49ab_Callback
         }, {
             "iconPosition": constants.ALERT_ICON_POSITION_LEFT
         })
--- studioactions/mobile/action_navigateTo_frmAdmitForm.js
+++ studioactions/mobile/action_navigateTo_frmAdmitForm.js
@@ -4,5 +4,5 @@ function action_navigateTo_frmAdmitForm() {
 
 function AS_NamedActions_92351225a2bc411e97fed2807a916c90() {
     frmAdmitForm.show();
-    closeMenu.call(this);
+    AS_NamedActions_59f90898340d4953be384a35208868b0();
 }
